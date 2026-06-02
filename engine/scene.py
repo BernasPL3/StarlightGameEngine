4eb65d46ad31fb0f471e79a4eb5b017613bd4528
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-02 17:43:07 -0300

    Create scene.py

---
+++ engine/scene.py
@@ -0,0 +1,14 @@
+class Scene:
+    def __init__(self):
+        self.objects = []
+
+    def add(self, obj):
+        self.objects.append(obj)
+
+    def update(self):
+        for obj in self.objects:
+            obj.update()
+
+    def draw(self, screen):
+        for obj in self.objects:
+            obj.draw(screen)
